caadceea17d6cea007ad879a2b90b40b04d651f4
Valentine Cheluchi <krixxvalle@gmail.com>
2025-06-29 22:45:03 +0100

    database

---
+++ task_4.sql
@@ -0,0 +1,16 @@
+USE alx_book_store;
+
+SELECT
+  COLUMN_NAME AS `Field`,
+  COLUMN_TYPE AS `Type`,
+  IS_NULLABLE AS `Null`,
+  COLUMN_KEY AS `Key`,
+  COLUMN_DEFAULT AS `Default`,
+  EXTRA AS `Extra`
+FROM
+  INFORMATION_SCHEMA.COLUMNS
+WHERE
+  TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books'
+ORDER BY
+  ORDINAL_POSITION;
